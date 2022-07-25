3973479d47cbd27e967770359f14715522acacc8
KapComrad <79967199+KapComrad@users.noreply.github.com>
2022-07-25 17:02:20 +0800

    WoW!

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -3,10 +3,9 @@ from asyncio.windows_events import NULL
 from msilib.schema import File
 from sys import getdefaultencoding
 import re
-import sys
 from this import s
+from tkinter import W
 import undetected_chromedriver as uc
-import requests
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 import time
@@ -20,6 +19,7 @@ def get_data_with_uc(url,name):
         driver = uc.Chrome(driver_executable_path='C:\\Users\\serg\\source\\repos\\PythonApplication3\\PythonApplication3\\chromedriver.exe',use_subprocess = True)
         driver.get(url=url)
         driver.implicitly_wait(20)
+        time.sleep(20)
         with open ('C:\\Users\\serg\source\\repos\\PythonApplication3\\PythonApplication3\\'+name+'.html' ,'w',encoding='utf-8') as file:
             print("opened")
             file.write(driver.page_source)
@@ -30,42 +30,73 @@ def get_data_with_uc(url,name):
         driver.close()
         driver.quit()
 
-
-
 def read_data_from_dm():
-    if (time.time() - os.path.getmtime("index.html")) > 1800:
+    if os.path.exists("index_dm.html") == False:
         get_data_with_uc("https://www.detmir.ru/catalog/index/name/suhie_smesi_i_zameniteli_moloka/brand/9341/stores-2341/age-0~5/", "index_dm")
-    if (time.time() - os.path.getmtime("data.csv")) > 1800:
-        with open('C:\\Users\\serg\source\\repos\\PythonApplication3\\PythonApplication3\\index.html', encoding='utf-8-sig') as file:
-            src = file.read()
-        if os.path.exists("data.csv"):
-            os.remove("data.csv")
-        soup = BeautifulSoup(src,"lxml")
-        all_products_hrefs = soup.find_all(class_="SY")
-        for item in all_products_hrefs:
-            discount_without_html = []
-            name_without_html = []
-            price_without_html = []
-            soup2 = BeautifulSoup(str(item),"lxml")
-            product_href = soup2.find_all(class_="SZ")
-            for item in product_href:
-                soup3 = BeautifulSoup(str(item),"lxml")
-                discount = soup3.find_all(class_="LK")
-                for item in discount:
-                    discount_without_html = re.findall(r'">([^"></]+)</',str(item))
-            for item in product_href:
-                name = soup3.find_all(class_="SU")
-                for item in name:
-                    name_without_html = re.findall(r'">([^"></]+)</',str(item))
-            for item in product_href:
-                price = soup3.find_all(class_="S_4")
-                for item in price:
-                    price_without_html = [item]
-            print(discount_without_html + name_without_html + price_without_html)
-            with open ("data.csv", "a", encoding="utf-8-sig") as file:
-                writer = csv.writer(file, lineterminator='\n')
-                writer.writerow((discount_without_html,name_without_html,price_without_html))
-            print("#######")
+    if (time.time() - os.path.getmtime("index_dm.html")) > 1800:
+        get_data_with_uc("https://www.detmir.ru/catalog/index/name/suhie_smesi_i_zameniteli_moloka/brand/9341/stores-2341/age-0~5/", "index_dm")
+    with open('C:\\Users\\serg\source\\repos\\PythonApplication3\\PythonApplication3\\index_dm.html', encoding='utf-8-sig') as file:
+        src = file.read()
+    if os.path.exists("data_dm.csv"):
+        os.remove("data_dm.csv")
+    soup = BeautifulSoup(src,"lxml")
+    all_products_hrefs = soup.find_all(class_="S_6")
+    for item in all_products_hrefs:
+        print("TUT?")
+        discount_without_html = []
+        name_without_html = []
+        price_without_html = []
+        soup2 = BeautifulSoup(str(item),"lxml")
+        product_href = soup2.find_all(class_="S_7")
+        for item in product_href:
+            soup3 = BeautifulSoup(str(item),"lxml")
+            name = soup3.find_all(class_="S_2")
+            for item in name:
+                name_without_html = re.findall(r'">([^"></]+)</',str(item))
+        for item in product_href:
+            price = soup3.find_all(class_="Td")
+            for item in price:
+                price_without_html = [item]
+        print(discount_without_html + name_without_html + price_without_html)
+        with open ("data_dm.csv", "a", encoding="utf-8-sig") as file:
+            writer = csv.writer(file, lineterminator='\n')
+            writer.writerow((discount_without_html,name_without_html,price_without_html))
+        print("#######")
+    replace_symbols_in_data_dm()
+
+def replace_symbols_in_data_dm():
+    if os.path.exists("data_dm.txt"):
+        os.remove("data_dm.txt")
+    with open ("C:\\Users\\serg\\source\\repos\\PythonApplication3\\PythonApplication3\\data_dm.csv", "r+", encoding="utf-8-sig") as file:
+        fin = open("data_dm.txt","w",encoding="utf-8-sig")
+        for line in file:
+            rep = ["[","]","'",'<p class=""S_4"">',"</p>",'"','<p class=Td>']
+            for item in rep:
+                if item in line:
+                    line = line.replace(item,'')
+            rep2 = [',']
+            for item2 in rep2:
+                if item2 in line:
+                    line = line.replace(item2,' ')
+            fin.write(line)
+
+def replace_symbols_in_data_lenta():
+    if os.path.exists("data_lenta.txt"):
+        os.remove("data_lenta.txt")
+    with open ("C:\\Users\\serg\\source\\repos\\PythonApplication3\\PythonApplication3\\data_lenta.csv", "r+", encoding="utf-8-sig") as file:
+        fin = open("data_lenta.txt","w",encoding="utf-8-sig")
+        for line in file:
+            rep = ['<span class=""price-label__integer"">','"[<div class=""sku-card-small-header__title"">','</div>]',"</p>",'</span>',"[","]","'",'"']
+            for item in rep:
+                if item in line:
+                    line = line.replace(item,'')
+            rep2 = [',']
+            for item2 in rep2:
+                    if item2 in line:
+                        line = line.replace(item2,' ')
+            fin.write(line)
+
+
 
 def read_data_from_lenta():
     if os.path.exists("index_lenta.html") == False:
@@ -95,41 +126,16 @@ def read_data_from_lenta():
             with open ("data_lenta.csv", "a", encoding="utf-8-sig") as file:
                 writer = csv.writer(file, lineterminator='\n')
                 writer.writerow((discount,name))
-        print("#######")
-
-
-
+    replace_symbols_in_data_lenta()
 
 def main():
+    print("PY")
     #read_data_from_dm()
-    read_data_from_lenta()
-
+    #replace_symbols_in_data_dm()
+    #read_data_from_lenta()
+    #replace_symbols_in_data_lenta()
 
 if __name__ == '__main__':
     main()
 
-#import time
-
-#from selenium import webdriver
-
-
-
-#driver = webdriver.Chrome('C:\\Users\\serg\source\\repos\\PythonApplication3\\PythonApplication3\\chromedriver.exe')  # Optional argument, if not specified will search path.
-
-#driver.get('http://www.google.com/');
-
-#time.sleep(5) # Let the user actually see something!
-
-#search_box = driver.find_element_by_name('q')
-
-#search_box.send_keys('ChromeDriver')
-#get_source = driver.page_source
-#fileToWrite = open('C:\\Users\\serg\source\\repos\\PythonApplication3\\PythonApplication3\\index.html' ,'w',encoding='utf-8')
-#print(get_source)
-#fileToWrite.write("hjhjhj")
-#fileToWrite.close()
-
-#search_box.submit()
-
-#time.sleep(5) # Let the user actually see something!
 
--- PythonApplication3/tg_bot.py
+++ PythonApplication3/tg_bot.py
@@ -30,24 +30,16 @@ async def get_all(message: types.Message):
 async def get_smec_DM(message: types.Message):
     await message.answer("Подождите , идет сбор актуальной информации")
     pya.read_data_from_dm()
-    with open ("data.csv", "r", encoding="utf-8-sig") as file:
+    with open ("data_dm.txt", "r", encoding="utf-8-sig") as file:
         for line in file:
-            rep = ["[","]","'",'<p class=""S_4"">',"</p>",'"']
-            for item in rep:
-                if item in line:
-                    line = line.replace(item,'')
             await message.answer(line)
 
 @dp.message_handler(Text(equals="Лента"))
 async def get_smec_DM(message: types.Message):
     await message.answer("Подождите , идет сбор актуальной информации")
     pya.read_data_from_lenta()
-    with open ("data_lenta.csv", "r", encoding="utf-8-sig") as file:
+    with open ("data_lenta.txt", "r", encoding="utf-8-sig") as file:
         for line in file:
-            rep = ['<span class=""price-label__integer"">','"[<div class=""sku-card-small-header__title"">','</div>]',"</p>",'</span>',"[","]","'"]
-            for item in rep:
-                if item in line:
-                    line = line.replace(item,'')
             await message.answer(line)
 
 @dp.message_handler(Text(equals="Назад"))
